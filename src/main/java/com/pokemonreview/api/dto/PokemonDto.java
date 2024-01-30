52635ef0bc6d9ff01b84473735001096f5bb1e1d
zky <zhengkanyan@gmail.com>
2024-01-30 14:51:21 +0800

    Added PokemonServiceTests

--- src/main/java/com/pokemonreview/api/dto/PokemonDto.java
+++ src/main/java/com/pokemonreview/api/dto/PokemonDto.java
@@ -1,7 +1,13 @@
 package com.pokemonreview.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Data
 public class PokemonDto {
     private int id;
